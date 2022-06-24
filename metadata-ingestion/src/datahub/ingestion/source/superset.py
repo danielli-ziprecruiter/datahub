8c2420383bf5a31dd5ecb65a598e9599f06580f3
Daniel Liu <danielli@ziprecruiter.com>
2022-06-24 12:24:24 -0400

    Allow superset ingestion without credentials

--- metadata-ingestion/src/datahub/ingestion/source/superset.py
+++ metadata-ingestion/src/datahub/ingestion/source/superset.py
@@ -123,27 +123,29 @@ class SupersetSource(Source):
         self.config = config
         self.report = SourceReport()
 
-        login_response = requests.post(
-            f"{self.config.connect_uri}/api/v1/security/login",
-            None,
-            {
-                "username": self.config.username,
-                "password": self.config.password,
-                "refresh": True,
-                "provider": self.config.provider,
-            },
-        )
-
-        self.access_token = login_response.json()["access_token"]
-
         self.session = requests.Session()
-        self.session.headers.update(
-            {
-                "Authorization": f"Bearer {self.access_token}",
-                "Content-Type": "application/json",
-                "Accept": "*/*",
-            }
-        )
+
+        if self.config.username:
+            login_response = requests.post(
+                f"{self.config.connect_uri}/api/v1/security/login",
+                None,
+                {
+                    "username": self.config.username,
+                    "password": self.config.password,
+                    "refresh": True,
+                    "provider": self.config.provider,
+                },
+            )
+
+            self.access_token = login_response.json()["access_token"]
+
+            self.session.headers.update(
+                {
+                    "Authorization": f"Bearer {self.access_token}",
+                    "Content-Type": "application/json",
+                    "Accept": "*/*",
+                }
+            )
 
         # Test the connection
         test_response = self.session.get(f"{self.config.connect_uri}/api/v1/database")
